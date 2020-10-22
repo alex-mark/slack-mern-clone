d37da0fa7e48c3c4e00a11b4662ae76b677d076c
Alex Markin <alexndr86@gmail.com>
2020-10-22 16:16:44 +0300

    Add user to context

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,18 +2,28 @@ import React from "react";
 import { Button } from "@material-ui/core";
 import "./Login.css";
 import { auth, provider } from "../firebase";
+import { useStateValue } from "../StateProvider";
+import { actionTypes } from "../reducer";
 
 function Login() {
+  const [state, dispatch] = useStateValue();
+
   const signIn = () => {
     auth
       .signInWithPopup(provider)
       .then((result) => {
         console.log(result);
+
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        });
       })
       .catch((error) => {
         alert(error.message);
       });
   };
+
   return (
     <div className="login">
       <div className="login__container">
