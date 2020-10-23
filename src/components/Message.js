ca4ac2d54168a2153849b95eee6c265f895e585f
Alex Markin <alexndr86@gmail.com>
2020-10-23 15:06:54 +0300

    Change icon

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,10 +4,10 @@ import "./Message.css";
 function Message({ message, timestamp, user, userImage }) {
   return (
     <div className="message">
-      <img src="userImage" alt="" />
+      <img src={userImage} alt="" />
       <div className="message__info">
         <h4>
-          {user}{" "}
+          {user}
           <span className="message__timestamp">
             {new Date(timestamp?.toDate()).toUTCString()}
           </span>
