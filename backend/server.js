180c14bad9a0cb9d9042c252b5d42934227edaff
Alex Markin <alexndr86@gmail.com>
2020-10-23 15:39:58 +0300

    Scaffold app

--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import Pusher from "pusher";
+
+// App config
+
+// Middlewares
+
+// db config
+
+// api routes
+
+// listen
