aaf0d3d804cc3e1b01f327f0f705e34c6a50d176
Alex Markin <alexndr86@gmail.com>
2020-10-24 16:38:06 +0300

    Add basic route

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,17 @@ import cors from "cors";
 import Pusher from "pusher";
 
 // App config
+const app = express();
+const port = process.env.PORT || 9000;
 
 // Middlewares
+app.use(cors());
+app.use(express.json());
 
 // db config
 
 // api routes
+app.get("/", (req, res) => res.status(200).send("Hello from Slack Clone api"));
 
 // listen
+app.listen(port, () => console.log(`listening on localhost:${port}`));
